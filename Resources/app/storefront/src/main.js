a0e985295c6c02ce24d91213184628377a1becbc
dieu do <do.do@shopware.com>
2020-12-01 09:50:06 +0700

    TSR-47 - Customized products

--- Resources/app/storefront/src/main.js
+++ Resources/app/storefront/src/main.js
@@ -1,8 +1,10 @@
 import LanguageCurrency from './plugin/language-currency/language-currency.plugin';
 import ShowroomCollapseColumn from './plugin/collapse-columns/collapse-columns.plugin';
 import ShowroomOffcanvasMenuPlugin from './plugin/main-menu/offcanvas-menu.plugin';
+import CustomizedProductsStepByStepWizard from './plugin/customized-product/customized-product.plugin';
 
 const PluginManager = window.PluginManager;
 PluginManager.register('LanguageCurrency', LanguageCurrency, '[data-language-currency]');
 PluginManager.register('ShowroomCollapseColumn', ShowroomCollapseColumn, '[data-showroom-collapse]');
 PluginManager.override('OffcanvasMenu', ShowroomOffcanvasMenuPlugin, '[data-offcanvas-menu]');
+PluginManager.override('SwagCustomizedProductsStepByStepWizard', CustomizedProductsStepByStepWizard, '*[data-swag-customized-product-step-by-step="true"]');
