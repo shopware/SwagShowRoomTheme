6a63001c62f631f43e411cd7728b7eca56ff5081
Dieu Do <d.do@shopware.com>
2020-12-16 12:16:05 +0700

    TSR-342 - CMS: Quickview clicking improvement in listing

--- Resources/app/storefront/src/main.js
+++ Resources/app/storefront/src/main.js
@@ -2,9 +2,11 @@ import LanguageCurrency from './plugin/language-currency/language-currency.plugi
 import ShowroomCollapseColumn from './plugin/collapse-columns/collapse-columns.plugin';
 import ShowroomOffcanvasMenuPlugin from './plugin/main-menu/offcanvas-menu.plugin';
 import CustomizedProductsStepByStepWizard from './plugin/customized-product/customized-product.plugin';
+import CmsExtensionsQuickviewOptions from './plugin/quick-view/quick-view.plugin';
 
 const PluginManager = window.PluginManager;
 PluginManager.register('LanguageCurrency', LanguageCurrency, '[data-language-currency]');
 PluginManager.register('ShowroomCollapseColumn', ShowroomCollapseColumn, '[data-showroom-collapse]');
 PluginManager.override('OffcanvasMenu', ShowroomOffcanvasMenuPlugin, '[data-offcanvas-menu]');
 PluginManager.override('SwagCustomizedProductsStepByStepWizard', CustomizedProductsStepByStepWizard, '*[data-swag-customized-product-step-by-step="true"]');
+PluginManager.override('SwagCmsExtensionsQuickview', CmsExtensionsQuickviewOptions, '[data-swag-cms-extensions-quickview="true"]');
