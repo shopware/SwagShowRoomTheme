02e19a8c92f332decab59c3c24a7927ae9f17773
Thu Vo <thu.vo@shopware.com>
2021-08-16 09:43:40 +0700

    TSR-456 - Shop page visual testing

---
+++ Resources/app/storefront/test/e2e/cypress/integration/workflow/error/maintence.spec.js
@@ -0,0 +1,59 @@
+describe('Error: maintenance page', { tags: ['@workflow'] }, () => {
+    beforeEach(() => {
+        cy.loginViaApi()
+            .then(() => {
+                cy.searchViaAdminApi({
+                    endpoint: 'sales-channel',
+                    data: {
+                        field: 'name',
+                        value: 'Storefront'
+                    }
+                });
+            })
+            .then((salesChannel) => {
+                // Enable Maintenance mode
+                cy.updateViaAdminApi('sales-channel', salesChannel.id, {
+                    data: {
+                        maintenance: true
+                    }
+                });
+            })
+            .then(() => {
+                cy.visit('/', { failOnStatusCode: false });
+            });
+    });
+
+    it('@workflow: enable maintenance mode', () => {
+        cy.get('.container-maintenance h2').contains('Maintenance mode');
+        cy.get('.container-maintenance p').contains('We are currently updating this site. Please check back later.');
+        cy.get('.container-maintenance img')
+            .first()
+            .should('have.attr', 'src')
+            .and('match', /maintenance/);
+
+        cy.get('.header-main').should('be.visible');
+        cy.get('.footer-simple').should('be.visible');
+
+        cy.loginViaApi()
+            .then(() => {
+                cy.searchViaAdminApi({
+                    endpoint: 'sales-channel',
+                    data: {
+                        field: 'name',
+                        value: 'Storefront'
+                    }
+                });
+            })
+            .then((salesChannel) => {
+                // Enable Maintenance mode
+                cy.updateViaAdminApi('sales-channel', salesChannel.id, {
+                    data: {
+                        maintenance: false
+                    }
+                });
+            })
+            .then(() => {
+                cy.visit('/', { failOnStatusCode: true });
+            });
+    });
+});
