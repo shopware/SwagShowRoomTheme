454caac1e81f1191a0a55a647f57f076f5b5c24e
Tuan Ngo <t.ngo@shopware.com>
2022-01-26 09:13:12 +0700

    NTR - prepare for platform v6.4.8.0

--- Resources/app/storefront/test/e2e/cypress/integration/workflow/theme-color.spec.js
+++ Resources/app/storefront/test/e2e/cypress/integration/workflow/theme-color.spec.js
@@ -87,12 +87,12 @@ describe('ThemeColor: Visual tests', () => {
 
         cy.get('.sw-colorpicker .sw-colorpicker__input').first().clear().typeAndCheck(colorScheme.primary);
 
-        cy.get('.sw-card__title').contains('E-Commerce')
+        cy.get('.sw-card__title').contains('eCommerce')
             .parent('.sw-theme-manager-detail__area')
             .find('.sw-colorpicker__input')
             .first().clear().typeAndCheck(colorScheme.buyButton);
 
-        cy.get('.sw-card__title').contains('E-Commerce')
+        cy.get('.sw-card__title').contains('eCommerce')
             .parent('.sw-theme-manager-detail__area')
             .find('.sw-colorpicker__input').eq(1).clear().typeAndCheck(colorScheme.price);
 
